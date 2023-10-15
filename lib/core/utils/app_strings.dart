d6a132a3af9e4ec21da938cce8c531fcb1b4805d
Khaled Ramadan <khaledramdan880@gmail.com>
2023-10-15 19:12:58 +0300

    App Strings

---
+++ lib/core/utils/app_strings.dart
@@ -0,0 +1,45 @@
+class AppStrings {
+  //! Choose Language
+  static const String chafApp = 'Chef App';
+  static const String welcome = 'مرحبا بك';
+  static const String chooseLang = 'من فضلك اختر لغتك';
+  static const String arabic = 'العربية';
+  static const String english = 'English';
+  //! Login Screen
+  static const String email = 'البريد الألكتروني';
+  static const String password = 'كلمة المرور';
+  static const String confirmPassword = 'تأكيد كلمة المرور';
+  static const String forgetPass = 'نسيت كلمة المرور';
+  static const String login = 'نسجيل الدخول';
+  static const String dontHaveEmail = 'ليس لديك حساب';
+  static const String createEmail = 'انشاء حساب';
+  //! Signup Screen
+    // Data
+    static const String name = 'الاسم';
+    static const String phoneNumber = 'رقم الهاتف';
+    static const String next = 'التالي';
+    static const String previous = 'السابق';
+    // Choose Picture 
+    static const String logoOrPicture = 'الشعار او الصورة الشخصية';
+    static const String choosePicture = 'أختر صورة أحسن من ألف صورة';
+    static const String camera = 'الكاميرا';
+    static const String gallery = 'المعرض';
+    // Location
+    static const String enterYourLocation = 'المعرض';
+    static const String location = 'المنطقة';
+    static const String address = 'العنوان';
+    static const String city = 'المدينة';
+    static const String governorate = 'المحافظة';
+    static const String requestsConditions = 'شروط الطلبات';
+    static const String minRequestsNum = 'الحد الأدني لقبول الطلبات';
+    // More data
+    static const String canWeKnowYouMore = 'ممكن نتعرف عليك اكتر؟';
+    static const String writeHere = 'اكتب هنا';
+    static const String requestsDocuments = 'المستندات المطلوبة';
+    static const String notionalIdFormFront = 'صورة الرقم القومي من الامام';
+    static const String notionalIdFormback = 'صورة الرقم القومي من الخلف';
+    static const String medicalCertificate = 'الشهادة الصحية';
+    static const String medicalCertificateCanExtractFromNerest =
+        'الشهادة الصحية ممكن تستخرجها بسهولة من لقرب مكتب صحة';
+    static const String save = 'حفظ';
+}
